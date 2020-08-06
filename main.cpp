e6e4c986691313fae5d968000e6665388d8273b5
Luka Matovic <ml170722d@student.etf.bg.ac.rs>
2020-08-06 18:45:08 +0200

    tested new linked list methods

--- main.cpp
+++ main.cpp
@@ -11,152 +11,13 @@
 
 #include "consts.h"
 
-/*
- * n = 270 max?
- * no more memory?
- */
-const int N = 5;
-/*
- void doSomething1() {
- Kernel::Lock::CS_lock();
- PCB* p[N];
-
- int i;
-
- for (i = 0; i < N; ++i) {
- p[i] = new PCB(1024, (i % 2) ? 50 : 100, NULL);
- p[i]->start();
- }
- Kernel::Lock::CS_unlock();
-
- /////////////////////////////////////////////////////////////
-
- Kernel::Lock::CS_lock();
- for (i = 0; i < N; i++) {
- cout << p[i] << " ";
- }
- cout << endl;
- Kernel::printAllPCB();
- Kernel::Lock::CS_unlock();
-
- /////////////////////////////////////////////////////////////
-
- for (i = 0; i < N; i++) {
- delete p[i];
- p[i] = NULL;
- }
- Kernel::Lock::CS_lock();
- cout << "all pcb terminated" << endl;
- Kernel::Lock::CS_unlock();
-
- /////////////////////////////////////////////////////////////
-
- Kernel::Lock::CS_lock();
- for (i = 0; i < N; i++) {
- cout << p[i] << " ";
- }
- cout << endl;
- Kernel::printAllPCB();
- Kernel::Lock::CS_unlock();
-
- /////////////////////////////////////////////////////////////
-
- Kernel::Lock::CS_lock();
- cout << "fin on main end = " << PCB::fin << endl;
- cout << "Happy end for doSomethig1()!" << endl;
- Kernel::Lock::CS_unlock();
- }
- */
-/*
-void waitSomeTime(int n) {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			for (int k = 0; k < n; k++) {
-
-			}
-		}
-	}
-}
-*/
-/*
-class TestThread: public Thread {
-public:
-	TestThread(StackSize ss, Time ts, int N) :
-			Thread(ss, ts) {
-		n = N;
-	}
-	;
-	~TestThread() {
-		waitToComplete();
-	}
-	static int fin;
-protected:
-
-	void run();
-
-private:
-	int n;
-};
-
-int TestThread::fin = 0;
-
-void TestThread::run() {
-
-	waitSomeTime(n);
-
-	Kernel::Lock::CS_lock();
-	cout<<"i am done, id: "<<getId()<<endl;
-	Kernel::Lock::CS_unlock();
-
-	fin++;
-}
-
-void doSomething2() {
-
-	Thread* t[N];
-
-	for (int i = 0; i < N; i++) {
-		t[i] = new TestThread(1024, (i%2)?50:100, (i%3)?2000:3000);
-		t[i]->start();
-	}
-
-	for (int j = N; j > 0; j--) {
-		delete t[j - 1];
-	}
-
-	Kernel::Lock::CS_lock();
-	cout << "fin on main end = " << TestThread::fin << endl;
-	cout << "Happy end for doSomethig2()!" << endl;
-	Kernel::Lock::CS_unlock();
-
-}
-*/
-/*
-class A: public Thread{
-public:
-	A():Thread(){}
-	~A(){
-		waitToComplete();
-	}
-protected:
-	virtual void run(){
-		cout<<"hi"<<endl;
-	}
-};
-
-void f(){
-
-}
-
-void doSomething3(){
-	userMain(0, NULL);
-}
-*/
+#include "linkLst.h"
 
 extern int syncPrintf(const char *format, ...);
 extern int userMain(int argc, char** argv);
 
 int main(int argc, char** argv) {
+
 
 	syncPrintf("Starting system\n");
 
@@ -167,6 +28,10 @@ int main(int argc, char** argv) {
 	Kernel::restore();
 
 	syncPrintf("System shutting down");
+
+
+	//tests_for_linkedList();
+
 	return 0;
 
 }
--- userMain.cpp
+++ userMain.cpp
@@ -7,46 +7,78 @@
 
 #include "thread.h"
 #include "kernel.h"
+#include "consts.h"
+#include "linkLst.h"
 
 void tick(){
 	cout<<".";
 }
 
-class TestThread : public Thread
-{
-private:
-	TestThread *t;
-
+class A:public Thread{
 public:
-
-	TestThread(TestThread *thread): Thread(), t(thread){}
-	~TestThread()
-	{
+	A(char c, Time ts):Thread(defaultStackSize, ts), znak(c){}
+	~A(){
 		waitToComplete();
 	}
 protected:
+	void run(){
+		for (unsigned long long int i = 0; i < 10000; ++i) {
+			kIntLock
+			cout<<znak;
+			kIntUnlock
+			for (int j = 0; j < 2000; ++j) {
+				for (int k = 0; k < 2000; ++k) {
 
-	void run();
-
+				}
+			}
+		}
+		kIntLock
+		cout<<endl<<znak<<" over"<<endl;
+		kIntUnlock
+	}
+private:
+	char znak;
 };
-
-void TestThread::run()
-{
-	t->waitToComplete();
-}
-
 
 int userMain(int argc, char** argv)
 {
-	syncPrintf("Test starts.\n");
-	TestThread *t1,*t2;
-	t1 = new TestThread(t2);
-	t2 = new TestThread(t1);
-	t1->start();
-	t2->start();
-	delete t1;
-	delete t2;
-	syncPrintf("Test ends.\n");
+
+	/*cout<<"test start"<<endl;
+
+	A *a = new A('a', 4),
+			*b = new A('b', 6),
+			*c = new A('c', 8),
+			*d = new A('d', 10),
+			*e = new A('e', 4),
+			*f = new A('f', 6),
+			*g = new A('g', 8),
+			*h = new A('h', 2);
+
+	a->start();
+	b->start();
+	c->start();
+	d->start();
+	e->start();
+	f->start();
+	g->start();
+	h->start();
+
+
+	delete a;
+	delete b;
+	delete c;
+	delete d;
+	delete e;
+	delete f;
+	delete g;
+	delete h;
+
+	cout<<"test over"<<endl;*/
+
+	lock_I;
+	tests_for_linkedList();
+	unlock_I;
+
 	return 0;
 }
 
